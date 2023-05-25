7cbaaa2543b4c77f28c65ef0e7094c4bdfc485a4
alfonso <alfonso1003@users.noreply.github.com>
2023-05-25 13:50:44 -0500

    generalize to calculator

--- calculator_command_event_history/commands.py
+++ calculator_command_event_history/commands.py
@@ -7,13 +7,7 @@ from calculator_command_event_history.chain_calculator import Calculator
 class CalculatorCommand(Protocol):
     """general protocol for calculator command"""
 
-    def execute(self, current_value: float) -> float:
-        pass
-
-    def undo(self, current_value: float) -> float:
-        pass
-
-    def redo(self, current_value: float) -> float:
+    def execute(self, calculator: Calculator) -> float:
         pass
 
 
@@ -24,24 +18,12 @@ class AddCommand:
     def execute(self, calculator: Calculator) -> float:
         calculator.add(self.value)
 
-    def undo(self, calculator: Calculator) -> float:
-        calculator.subtract(self.value)
-
-    def redo(self, calculator) -> float:
-        calculator.add(self.value)
-
 
 @dataclass
 class SubtractCommand:
     value: float = 0.0
 
     def execute(self, calculator: Calculator) -> float:
-        calculator.subtract(self.value)
-
-    def undo(self, calculator: Calculator) -> float:
-        calculator.add(self.value)
-
-    def redo(self, calculator: Calculator) -> float:
         calculator.subtract(self.value)
 
 
@@ -52,24 +34,12 @@ class MultiplyCommand:
     def execute(self, calculator: Calculator) -> float:
         calculator.multiply(self.value)
 
-    def undo(self, calculator: Calculator) -> float:
-        calculator.divide(self.value)
-
-    def redo(self, calculator: Calculator) -> float:
-        calculator.multiply(self.value)
-
 
 @dataclass
 class DivideCommand:
     value: float = 0.0
 
     def execute(self, calculator: Calculator) -> float:
-        calculator.divide(self.value)
-
-    def undo(self, calculator: Calculator) -> float:
-        calculator.multiply(self.value)
-
-    def redo(self, calculator: Calculator) -> float:
         calculator.divide(self.value)
 
 
@@ -80,14 +50,4 @@ class BatchCommand:
     def execute(self, current_value: float) -> float:
         for command in self.commands:
             current_value = command.execute(current_value)
-        return current_value
-
-    def undo(self, current_value: float) -> float:
-        for command in reversed(self.commands):
-            current_value = command.undo(current_value)
-        return current_value
-
-    def redo(self, current_value: float) -> float:
-        for command in self.commands:
-            current_value = command.redo(current_value)
         return current_value
--- calculator_command_event_history/controller.py
+++ calculator_command_event_history/controller.py
@@ -8,30 +8,31 @@ from calculator_command_event_history.chain_calculator import Calculator
 from calculator_command_event_history.commands import CalculatorCommand
 
 
-class ChainCalculatorController:
+class CalculatorController:
     def __init__(self, calculator: Calculator):
         self._calculator = calculator
         self._history: List[CalculatorCommand] = []
         self._current_idx: int = 0
 
-    def register(self, command: CalculatorCommand) -> ChainCalculatorController:
+    def register(self, command: CalculatorCommand) -> CalculatorController:
         del self._history[self._current_idx :]
         self._history.append(command)
         self._current_idx += 1
         return self
 
-    def undo(self) -> ChainCalculatorController:
+    def undo(self) -> CalculatorController:
         if self._current_idx:
             self._current_idx -= 1
         return self
 
-    def redo(self) -> ChainCalculatorController:
+    def redo(self) -> CalculatorController:
         if self._current_idx < len(self._history):
             self._current_idx += 1
         return self
 
     @property
     def calculator_total(self):
+        self._calculator.reset_total()
         for command in self._history[: self._current_idx]:
             command.execute(self._calculator)
         return self._calculator.total
