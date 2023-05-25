6033addd4b02b4774c3f30c7ad62454461005aee
alfonso <alfonso1003@users.noreply.github.com>
2023-05-25 01:47:50 -0500

    test suite

---
+++ tests/test_calculator_command_state.py
@@ -0,0 +1,176 @@
+import pytest
+
+from calculator_command_state.chain_calculator import ChainCalculator
+from calculator_command_state.commands import (
+    AddCommand,
+    DivideCommand,
+    MultiplyCommand,
+    SubtractCommand,
+)
+from calculator_command_state.controller import ChainCalculatorController
+
+
+class TestChainCalculator:
+    @classmethod
+    def setup_method(cls):
+        cls.calculator = ChainCalculator()
+        cls.calculator_initial_value = ChainCalculator(100)
+
+        cls.calculator_controller = ChainCalculatorController(cls.calculator)
+        cls.calculator_controller_initial_value = ChainCalculatorController(
+            cls.calculator_initial_value
+        )
+
+    def test_initialize_total(self):
+        assert self.calculator.total == 0
+        assert self.calculator_initial_value.total == 100
+
+    def test_addition(self):
+        self.calculator_controller.execute(AddCommand(100))
+        result = self.calculator_controller.calculator.total
+        assert result == 100
+
+    def test_addition_initial_value(self):
+        self.calculator_controller_initial_value.execute(AddCommand(100))
+        result = self.calculator_controller_initial_value.calculator.total
+        assert result == 200
+
+    def test_subtraction(self):
+        self.calculator_controller.execute(SubtractCommand(100))
+        result = self.calculator_controller.calculator.total
+        assert result == -100
+
+    def test_subtraction_initial_value(self):
+        self.calculator_controller_initial_value.execute(SubtractCommand(100))
+        result = self.calculator_controller_initial_value.calculator.total
+        assert result == 0
+
+    def test_multiplication(self):
+        self.calculator_controller.execute(MultiplyCommand(100))
+        result = self.calculator_controller.calculator.total
+        assert result == 0
+
+    def test_multiplication_inital_value(self):
+        self.calculator_controller_initial_value.execute(MultiplyCommand(100))
+        result = self.calculator_controller_initial_value.calculator.total
+        assert result == 10000
+
+    def test_division(self):
+        self.calculator_controller.execute(DivideCommand(100))
+        result = self.calculator_controller.calculator.total
+        assert result == 0
+
+    def test_division_inital_value(self):
+        self.calculator_controller_initial_value.execute(DivideCommand(100))
+        result = self.calculator_controller_initial_value.calculator.total
+        assert result == 1
+
+    def test_division_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calculator_controller_initial_value.execute(DivideCommand(0))
+
+
+class TestChainCalculatorChaining:
+    @classmethod
+    def setup_method(cls):
+        cls.calculator = ChainCalculator()
+        cls.calculator_controller = ChainCalculatorController(cls.calculator)
+
+    def test_chaining(self):
+        (
+            self.calculator_controller.execute(AddCommand(100))
+            .execute(MultiplyCommand(2))
+            .execute(SubtractCommand(50))
+            .execute(DivideCommand(5))
+        )
+        result = self.calculator_controller.calculator.total
+        assert result == 30
+
+    def test_chaining_undo(self):
+        (
+            self.calculator_controller.execute(AddCommand(100))
+            .execute(MultiplyCommand(2))
+            .execute(SubtractCommand(50))
+            .execute(DivideCommand(5))
+        )
+        result = self.calculator_controller.calculator.total
+        assert result == 30
+
+        self.calculator_controller.undo().undo().undo().undo()
+        result = self.calculator_controller.calculator.total
+        assert result == 0
+
+        self.calculator_controller.redo().redo().redo().redo()
+        result = self.calculator_controller.calculator.total
+        assert result == 30
+
+
+class TestChainCalculatorUndoRedo:
+    @classmethod
+    def setup_method(cls):
+        cls.calculator = ChainCalculator()
+        cls.calculator_initial_value = ChainCalculator(100)
+
+        cls.calculator_controller = ChainCalculatorController(cls.calculator)
+        cls.calculator_controller_initial_value = ChainCalculatorController(
+            cls.calculator_initial_value
+        )
+
+    def test_addition_undo_redo(self):
+        self.calculator_controller.execute(AddCommand(100))
+        result = self.calculator_controller.calculator.total
+        assert result == 100
+
+        self.calculator_controller.undo()
+        result_undo = self.calculator_controller.calculator.total
+        assert result_undo == 0
+
+        self.calculator_controller.redo()
+        result_redo = self.calculator_controller.calculator.total
+        assert result_redo == 100
+
+    def test_subtraction_undo_redo(self):
+        self.calculator_controller.execute(SubtractCommand(100))
+        result = self.calculator_controller.calculator.total
+        assert result == -100
+
+        self.calculator_controller.undo()
+        result_undo = self.calculator_controller.calculator.total
+        assert result_undo == 0
+
+        self.calculator_controller.redo()
+        result_redo = self.calculator_controller.calculator.total
+        assert result_redo == -100
+
+    def test_multiplication_undo_redo(self):
+        self.calculator_controller_initial_value.execute(MultiplyCommand(2))
+        result = self.calculator_controller_initial_value.calculator.total
+        assert result == 200
+
+        self.calculator_controller_initial_value.undo()
+        result_undo = self.calculator_controller_initial_value.calculator.total
+        assert result_undo == 100
+
+        self.calculator_controller_initial_value.redo()
+        result_redo = self.calculator_controller_initial_value.calculator.total
+        assert result_redo == 200
+
+    def test_division_undo_redo(self):
+        self.calculator_controller_initial_value.execute(DivideCommand(2))
+        result = self.calculator_controller_initial_value.calculator.total
+        assert result == 50
+
+        self.calculator_controller_initial_value.undo()
+        result_undo = self.calculator_controller_initial_value.calculator.total
+        assert result_undo == 100
+
+        self.calculator_controller_initial_value.redo()
+        result_redo = self.calculator_controller_initial_value.calculator.total
+        assert result_redo == 50
+
+    def test_division_by_zero_undo(self):
+        """Can't undo and throws division by zero error! 😔"""
+        self.calculator_controller_initial_value.execute(MultiplyCommand(0))
+
+        with pytest.raises(ZeroDivisionError):
+            self.calculator_controller_initial_value.undo()
